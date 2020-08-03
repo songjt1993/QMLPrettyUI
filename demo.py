33b4f34469ca7f349069277674b2a7213010060f
Maybe <songjingtao_abc@163.com>
2020-08-03 19:52:42 +0800

    图表ui

--- demo.py
+++ demo.py
@@ -1,16 +1,25 @@
 # -*-coding: utf-8-*-
 import sys, os
 from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQuick import QQuickView
-from PyQt5.QtGui import QGuiApplication
 
 
-app = QGuiApplication(sys.argv)
+# app = QGuiApplication(sys.argv)
+app = QApplication(sys.argv)
+
 view = QQuickView()
 view.setResizeMode(QQuickView.SizeRootObjectToView)
 view.engine().quit.connect(app.quit)
-view.setSource(QUrl("./ImageBrowser/ImageBrowser.qml"))
-for i in range(5):
-    view.rootObject().addImage("../resources/ImageBrowser/{}.jpg".format(i+1))
+
+# ImageBrowser 
+# view.setSource(QUrl("./ImageBrowser/ImageBrowser.qml"))
+# for i in range(5):
+#     view.rootObject().addImage("../resources/ImageBrowser/{}.jpg".format(i+1))
+
+# NotJustLineChart
+view.setTitle("NotJustLineChart")
+view.setSource(QUrl("./NotJustLineChart/NotJustLineChart.qml"))
+
 view.show()
 sys.exit(app.exec_())
